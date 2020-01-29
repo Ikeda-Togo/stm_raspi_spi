f5c2385c09b5093795b854c20a038b14bfa95f47
IkedaTogo <47003257+Ikeda-Togo@users.noreply.github.com>
2020-01-29 22:43:27 +0900

    foooooo

--- gyro/flag.py
+++ gyro/flag.py
@@ -1,4 +1,6 @@
-def flag_get(ay,ax): 
+import math
+
+def flag_get(ax,ay,az): 
     if ax<0:
         flag1=0
     if ax>=0:
@@ -7,15 +9,24 @@ def flag_get(ay,ax):
         flag2=0
     if ay>=0:
         flag2=1
+    
+    print("f1={0},f2={1}".format(flag1,flag2))
 
 
 
-    if flag1==0 & flag2==1:
-        ax=-ax
-    if flag1==0 & flag2==0:
+    if flag1==0 and flag2==1:
+        pitch = -math.atan(ax / math.sqrt( ay* ay+ az*az ) ) * 57.324
+        print("----1----")
+    elif flag1==0 and flag2==0:
+        pitch = 180+math.atan(ax / math.sqrt( ay* ay+ az*az ) ) * 57.324
         print("----2----")
-        ax=2-ax
-    if flag1==1 & flag2==0:
-        ax=2+ax
-    if flag1==1 & flag2==1:
-        ax=4-ax
+    elif flag1==1 and flag2==0:
+        pitch = 180+math.atan(ax / math.sqrt( ay* ay+ az*az ) ) * 57.324
+        print("----3----")
+    elif flag1==1 and flag2==1:
+        pitch = 360-math.atan(ax / math.sqrt( ay* ay+ az*az ) ) * 57.324
+        print("----4----")
+        
+    return pitch
+        
+        
